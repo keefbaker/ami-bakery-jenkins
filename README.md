07b19eb7053f010790334a46cc59c5413dc4a201
Keith Baker <keef.baker@n5md.com>
2021-02-06 20:34:00 +0000

    lint changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ami-bakery-jenkins
 
+![Test](https://github.com/keefbaker/ami-bakery-jenkins/workflows/Test/badge.svg)
+
 This is an example of how to create an AMI Bakery that can update the ec2 plugin when completed.
 
 It will also clean up old AMIs that are out of date.
--- inspec/controls/nginx.rb
+++ inspec/controls/nginx.rb
@@ -1,11 +1,11 @@
-control 'check nginx is installed and running' do
-  describe service('nginx') do
+control "check nginx is installed and running" do
+  describe service("nginx") do
     it { should be_installed }
     it { should be_enabled }
     it { should be_running }
   end
   describe port(80) do
     it { should be_listening }
-    its('processes') {should include 'nginx'}
+    its("processes") { should include "nginx" }
   end
 end
