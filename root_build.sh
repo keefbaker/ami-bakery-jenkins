5d887e3bcd05fcbfda96bbf69236fc084a3c20cf
Keith Baker <keef.baker@n5md.com>
2021-02-10 12:17:06 +0000

    Update root_build.sh

--- root_build.sh
+++ root_build.sh
@@ -3,5 +3,6 @@ yum update -y
 sudo yum install -y amazon-linux-extras
 amazon-linux-extras enable nginx1
 yum install -y nginx
+yum install -y java-1.8.0-openjdk
 systemctl enable nginx
 systemctl start nginx
