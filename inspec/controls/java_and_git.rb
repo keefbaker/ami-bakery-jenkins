4be79ec3e069368b6262e2cfda103cac2a3d0cf6
Keith Baker <keith.baker@arm.com>
2021-02-16 06:43:00 +0000

    missing end
    Change-Id: I6f44d4c95ff96928c9ce63b21649a027c160094a

--- inspec/controls/java_and_git.rb
+++ inspec/controls/java_and_git.rb
@@ -4,7 +4,8 @@ control "check java and git are installed" do
   # test the service and the port
   installed_packages = ["java-1.8.0-openjdk", "git"]
   installed_packages.each do |installed_package|
-  describe package(installed_package) do
-    it { should be_installed }
+    describe package(installed_package) do
+      it { should be_installed }
+    end
   end
 end
